81acd08f582033d854d9872053dd31ded3e4ad78
hstrantor <barry.lg.62@gmail.com>
2015-11-04 06:26:33 +0000

    added config file and setup script

--- set_up.py
+++ set_up.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python
 
-# parsing largely inspired by an article on cyberciti.biz
+# check for necessary modules on system
+try:
+    import sys
+    sys_loaded = True
+except ImportError:
+    sys_loaded = False
+try:
+    import cherrypy
+    cherrypy_loaded = True
+except ImportError:
+    cherrypy_loaded = False
+try:
+    import json
+    json_loaded = True
+except ImportError:
+    json_loaded = False
+try:
+    import server  # change name of api module
+    server_loaded = True
+except ImportError:
+    server_loaded = False
 
-# TODO check for necessary modules on system
-import cherrypy
-import argparse
-import server
+modules = {'sys':sys_loaded, 'cherrypy':cherrypy_loaded,
+           'json':json_loaded, 'server':server_loaded}
+for key in modules:
+    if modules[key] == False:
+        print "errer msg bc specified module wouldnt load"
+        sys.exit(1)
+
+#import argparse
 
 def main():
+    """
     parser = argparse.ArgumentParser(description="Parser for API setup script")
     # not '-h', bc this is autoset to 'help'
     parser.add_argument('-l', '--host', help="HTTP HOST", required=True)
@@ -16,16 +41,15 @@ def main():
     args = parser.parse_args()
 
     print "just a test", args.host, args.port, args.timeout
-
+    """
     # 1) load config from conf file into dict
     # 2) apply changes from cmd-args
+    #conf = None
     # 3) run app
     
-    cherrypy.tree.mount(
-            server.Objects(), '/api/objects',
-            {'/': { 'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-            }
-    )
+    cherrypy.tree.mount( server.Objects(),
+                         '/api/objects',
+                         config="./cherrypy.conf")
 
     # start cherrypy engine
     cherrypy.engine.start()
